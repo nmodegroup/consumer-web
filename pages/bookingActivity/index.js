a22b983afe5ffcd409548f5df7b6c9cbe8f87881
yangjing <yangjing@benpaobao.com>
2019-10-09 17:08:55 +0800

    update

--- pages/bookingActivity/index.js
+++ pages/bookingActivity/index.js
@@ -166,12 +166,5 @@ Page({
    */
   onReachBottom: function () {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -333,5 +333,13 @@ Page({
    */
   onReachBottom: function () {
     
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function (res) {
+    return {
+      path: 'pages/index/index'
+    }
   }
 })
