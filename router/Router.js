5cc66c7b83f27e98cac8e1710ee8d9a6be98afc5
kgm1818 <1634750252@qq.com>
2019-12-11 21:08:00 +0800

    订单页开发

--- router/Router.js
+++ router/Router.js
@@ -13,3 +13,4 @@ export const Collect = '/pages/collect/index'// 我的收藏
 export const Search = '/pages/search/index'// 搜索
 export const Setting = '/pages/setting/index'// 授权页面
 export const Protocol = '/pages/protocol/protocol'// 用户协议页面
+export const Buy = '/pages/buy/buy'// 确认订单页
