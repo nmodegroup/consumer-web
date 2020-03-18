82374158ae2de896b5c8c330ef5386789a88df93
kgm1818 <1634750252@qq.com>
2020-03-18 22:29:17 +0800

    image oss

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ App({
       nickName: ''
     },
     token: '',
-    baseImgUrl: ENV.imgHost + '/source/',//设置图片域名路径
+    baseImgUrl: ENV.imgHost + '/',//设置图片域名路径
     cid: 440300,//选取城市的id 默认是深圳的id
     phone: '',//当前用户手机号
     online: false, //是否登录
